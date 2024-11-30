2b019b2f63597720c911b71a62b1a88dc461f665
Terra <bieyuanxi@163.com>
2024-11-30 19:54:27 +0800

    .gitignore

---
+++ test_ofdm.py
@@ -0,0 +1,19 @@
+import unittest
+
+import numpy as np
+
+from ofdm import ofdm_modulate
+from zc import generate_zc_sequence
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        Nzc = 81
+        zc = generate_zc_sequence(1, Nzc)
+        x = ofdm_modulate(zc, 960)
+
+        self.assertEqual(True, np.all(x.imag < 1e-10))  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
