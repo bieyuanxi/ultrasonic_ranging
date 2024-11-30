f9c684d52950323e8537cd0d7193d9a14950ae1b
Terra <bieyuanxi@163.com>
2024-11-30 20:15:07 +0800

    add cmt 2 ofdm_modulate

--- ofdm.py
+++ ofdm.py
@@ -4,9 +4,13 @@ import numpy as np
 def ofdm_modulate(zc, N: int):
     Nzc = len(zc)
     hzc = (Nzc - 1) // 2
+    # Perform Nzc-point DFT on zc to get ZC
     ZC = np.fft.fft(zc, Nzc)
+
+    # Frequency domain rearrangement
     ZC = np.roll(ZC, hzc)
-    # print(np.dot(ZC, ZC.conjugate()))
+
+    ### OFDM modulation ###
     X = np.zeros(N, dtype=complex)
     X[380 - hzc: 380 + hzc + 1] = ZC    # FIXME: 380 should be replaced with N * fc / fs!!!
 
@@ -14,7 +18,10 @@ def ofdm_modulate(zc, N: int):
     X_half = X[1:(N + 1) // 2]
     X_half_conj = X_half[::-1].conj()
     X[N - N // 2 + 1:] = X_half_conj
+    ### End of OFDM modulation ###
 
     assert np.array_equal(X[1:].conj(), X[:0:-1]) # 验证是否是共轭对称
+
+    # Perform N-point IFFT on X to get x
     x = np.fft.ifft(X)
     return x
